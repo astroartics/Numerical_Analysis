725087a4b8e1864c2837ab3456d98fff81dab730
Sanjana <joshisanjanana114@gmail.com>
2025-01-11 02:25:39 +0530

    Update

--- Numerical_Analysis_Lib.cpp
+++ Numerical_Analysis_Lib.cpp
@@ -1,5 +1,72 @@
 #include <iostream>
+#define MARGIN 0.01
 
-class Numerical_Analysis{
+using namespace std;
 
+class BisectionMethod
+{
+public:
+    double f(double x)
+    {
+        return (x * x * x) - (3 * x) - 5;
+    }
+
+public:
+    void root()
+    {
+        double a, b;
+
+        int flag = 0;
+        for (int i = 1; i < 50; i++)
+        {
+            for (int j = 2; j < 50; j++)
+            {
+                if (f(i) < 0.0 && f(j) > 0.0)
+                {
+                    flag = 1;
+                    a = i;
+                    b = j;
+                    cout << a << " " << b << ":" << f(a) << " " << f(b) << endl;
+                    break;
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+            if (flag)
+            {
+                break;
+            }
+        }
+
+        double c = a;
+        while ((b - a) >= MARGIN)
+        {
+            c = (a + b) / 2;
+
+            if (f(c) == 0.0)
+            {
+                break;
+            }
+
+            if ((f(a) > 0 && f(c) < 0) || (f(a) < 0 && f(c) > 0))
+            {
+                b = c;
+            }
+            else
+            {
+                a = c;
+            }
+        }
+        cout << "The value of root is : " << c << endl;
+    }
+};
+
+int main()
+{
+    BisectionMethod bisection;
+    bisection.root();
+
+    return 0;
 }
