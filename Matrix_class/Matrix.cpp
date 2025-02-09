1a315ef63503febebd19b74e74a643f032704da9
Sanjana <joshisanjanana114@gmail.com>
2025-02-09 23:55:14 +0530

    Update

--- Matrix_class/Matrix.cpp
+++ Matrix_class/Matrix.cpp
@@ -1,5 +1,13 @@
 #include "Matrix.hpp"
 
+using namespace std;
+
+Matrix::Matrix(int r, int c)
+{
+    rows = r;
+    cols = c;
+}
+
 Matrix::Matrix()
 {
     mat = new double *[rows]; // Double pointer pointing to the first element of array of pointers -> that points to the rows of the matrix
@@ -7,4 +15,24 @@ Matrix::Matrix()
     {
         mat[i] = new double[cols]; // mat[i] pointer points to the first element of the ith row with 'cols' columns.
     }
+}
+
+Matrix::Matrix(string filename)
+{
+    ifstream matFile(filename);
+
+    if (!matFile)
+    {
+        cout << "Error opening the matrix file!" << endl;
+        exit(0);
+    }
+}
+
+Matrix::~Matrix()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] mat[i]; // De-allocating the memory held by row pointers first
+    }
+    delete[] mat; // Then de-allocating the memory held by the double pointer
 }
--- Matrix_class/Matrix.hpp
+++ Matrix_class/Matrix.hpp
@@ -1,9 +1,16 @@
 #include <cmath>
 #include <bits/stdc++.h>
+#include <string>
+#include <fstream>
 
 class Matrix
 {
 public:
     int rows, cols;
     double **mat;
+
+    Matrix(int, int);
+    Matrix();
+    Matrix(std::string filename);
+    ~Matrix();
 };
