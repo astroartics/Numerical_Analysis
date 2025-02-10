01be3b11ce4742db11aa5cee05f205481503012c
Sanjana <joshisanjanana114@gmail.com>
2025-02-10 17:37:24 +0530

    Update

--- Matrix_class/main.cpp
+++ Matrix_class/main.cpp
@@ -77,6 +77,6 @@ int main()
 
     // Multiply
     cout << "Multiplication : " << endl;
-    result = identity.multiply(symmetric);
+    result = defMat.multiply(symmetric);
     result.displayMat();
 }
