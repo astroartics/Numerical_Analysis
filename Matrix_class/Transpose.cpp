276ff381dfd4604d700ba3d374a02ea0ba434a82
Sanjana <joshisanjanana114@gmail.com>
2025-02-24 21:26:03 +0530

    Update

--- Matrix_class/Transpose.cpp
+++ Matrix_class/Transpose.cpp
@@ -23,23 +23,3 @@ Matrix Matrix::transpose()
 
     return transpose;
 }
-
-/*
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-
-        vector<vector<int>> transpose(cols, vector<int> (rows,0));
-
-        int a = 0, b = 0;
-        for(int i = 0; i < cols; i++)
-        {
-            for(int j=0;j<rows;j++)
-            {
-                transpose[a][b] = matrix[j][i];
-                b = (b+1) % rows;   // Change row of transpose till it reaches the last row
-                if(b==0) a++;   // Change column of transpose
-            }
-        }
-
-        return transpose;
-*/
