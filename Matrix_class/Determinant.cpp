455879118b63636ba8cbcde16d99fe78dc113c16
Sanjana <joshisanjanana114@gmail.com>
2025-03-05 15:49:32 +0530

    Update

--- Matrix_class/Determinant.cpp
+++ Matrix_class/Determinant.cpp
@@ -4,14 +4,4 @@ using namespace std;
 
 void Matrix::determinant(int n)
 {
-    int sum = 0;
-    int M = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            
-            sum += (mat[i][j] * M);
-        }
-    }
 }
