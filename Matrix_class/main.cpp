fe27c5b9d1437a4cc1658e7ed2a6b08ec55929ff
Sanjana <joshisanjanana114@gmail.com>
2025-03-07 11:42:45 +0530

    Update

--- Matrix_class/main.cpp
+++ Matrix_class/main.cpp
@@ -95,18 +95,6 @@ int main()
     {
         cout << "x" << (i + 1) << " : " << ans[i] << endl;
     }
-
-    // n = 4;
-    // Matrix CMat(n, n);
-    // double tempC[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-    // for (int i = 0; i < n; ++i)
-    // {
-    //     for (int j = 0; j < n; ++j)
-    //     {
-    //         CMat.mat[i][j] = tempC[i][j];
-    //     }
-    // }
-    // CMat.determinant(n);
 }
 
 /*
